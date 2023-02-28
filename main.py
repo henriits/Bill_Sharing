b84907327b5fda76d01b38f976059861df3857bd
henriits <121551949+henriits@users.noreply.github.com>
2023-02-28 09:30:36 +0200

    flatmate class start

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ class Bill:
     Object that contains data about a bill,
     such as total amount and period of the bill.
     """
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    Creates a flatmate person who lives in the flat
+    and pays the share of the bill.
+    """
