652fcc022c31945a927598d27d1b1f873a0960fd
henriits <121551949+henriits@users.noreply.github.com>
2023-02-28 09:33:08 +0200

    pass method

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ class Bill:
     Object that contains data about a bill,
     such as total amount and period of the bill.
     """
+
     def __init__(self, amount, period):
         self.amount = amount
         self.period = period
@@ -13,3 +14,10 @@ class Flatmate:
     Creates a flatmate person who lives in the flat
     and pays the share of the bill.
     """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        pass
