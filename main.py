49d5b5da90f9c45dbcab762ad2adc2dd56eb687b
henriits <121551949+henriits@users.noreply.github.com>
2023-03-01 17:23:16 +0200

    pdf report class

--- main.py
+++ main.py
@@ -21,3 +21,16 @@ class Flatmate:
 
     def pays(self, bill):
         pass
+
+
+class PdfReport:
+    """
+    Creates a PDf file that contains data about the flatmates such as their names, their due amount and the period of the bill
+
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
