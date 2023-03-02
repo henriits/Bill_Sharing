e05b812c2225af1158061ce67b73e0fa252af106
henriits <121551949+henriits@users.noreply.github.com>
2023-03-02 19:48:24 +0200

    added image to pdf

--- main.py
+++ main.py
@@ -46,6 +46,9 @@ class PdfReport:
         pdf = FPDF(orientation="P", unit="pt", format="A4")
         pdf.add_page()
 
+        # Add icon
+        pdf.image("house.png", w=30,h=30)
+
         # Insert Title
         pdf.set_font(family="Times", size=24, style="B")
         pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
